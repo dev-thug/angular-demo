95dcdce47bd2dffa1a90bbfe074939668c82772d
HJ <de0978@gmail.com>
2021-11-15 23:12:22 +0900

    add 플렉스 레이아웃 실습

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppComponent} from './app.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {MatButtonModule} from "@angular/material/button";
     BrowserModule,
     MatToolbarModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
